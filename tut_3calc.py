53864032d2e1fdb02f99ce1c271c0292f2d938f1
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-11 12:57:42 +0500

    simple calculator

---
+++ tut_3calc.py
@@ -0,0 +1,20 @@
+# addition
+print (2+3)
+
+# subtraction
+print (2-3)
+
+# multiplication
+print (2*3)
+
+# flost division
+print (4/2)
+
+# integer division
+print (4//2)
+
+# modulus, it gives remainder
+print (4%3)
+
+# exponent
+print (2**3)
