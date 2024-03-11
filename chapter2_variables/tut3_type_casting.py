e4c4921cdfdd4d1b16cc67a7b78e9a1a09882247
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-11 14:35:46 +0500

    type casting in python

---
+++ chapter2_variables/tut3_type_casting.py
@@ -0,0 +1,17 @@
+num = 3
+num_to_str = str(num)
+
+print(type(num))  # Output: <class 'int'>
+print(type(num_to_str))  # Output: <class 'str'>
+
+num_str = "3"
+
+# Convert string to an integer
+num_int = int(num_str)
+print(num_int)  # Output: 3
+print(type(num_int))  # Output: <class 'int'>
+
+# Convert string to a float
+num_float = float(num_str)
+print(num_float)  # Output: 3.0
+print(type(num_float))  # Output: <class 'float'>
