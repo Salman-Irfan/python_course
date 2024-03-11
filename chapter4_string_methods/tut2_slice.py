7b906b612c7f4ce8489a815a00714a581f6591c5
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-11 16:12:06 +0500

    slice step argument

--- chapter4_string_methods/tut2_slice.py
+++ chapter4_string_methods/tut2_slice.py
@@ -1,4 +1,9 @@
 # slicing / selecting sub-sequences
 language = "python"
-# syntax - [start argument : stop argument]
+# syntax - [start argument : stop argument-1]
 print(language[0:2])
+
+language2 = "javaScript"
+# syntax - [start argument : stop argument-1: step argument]
+print(language2[0:7])
+print(language2[0:7:2])
