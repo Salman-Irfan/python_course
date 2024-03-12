50e5f1031d14d22ee6263dcc0f733e4f25482f52
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-12 10:09:01 +0500

    not operator

---
+++ chapter5_if_else/tut4_not.py
@@ -0,0 +1,10 @@
+# Dummy user data
+user_data = {"name": "Alice", "age": 25, "agree_to_terms": False}
+
+# Check if the user has agreed to the terms and conditions
+if not user_data["agree_to_terms"]:
+    print(
+        f"{user_data['name']}, you must agree to the terms and conditions to proceed."
+    )
+else:
+    print(f"Welcome, {user_data['name']}! You have agreed to the terms and conditions.")
