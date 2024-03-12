80916de8d6980f1022b201288c5db6cc756d4d9f
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-12 12:44:32 +0500

    step argument in range function

--- chapter6_loops/tut3_for_range.py
+++ chapter6_loops/tut3_for_range.py
@@ -5,3 +5,6 @@ for i in range(10): # 0 - 9
 for j in range(5,15): # 5 - 14
     print(f"value of j is : {j}")
     
+# step argument in range function
+for k in range(1,10,3):
+    print(f"value of k is : {k}")
