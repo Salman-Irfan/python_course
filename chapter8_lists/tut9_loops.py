5b5fafe1b7e2cb715f6a5c6cebb0d2c5fc320b29
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-13 21:04:21 +0500

    for and while loop in a list

---
+++ chapter8_lists/tut9_loops.py
@@ -0,0 +1,14 @@
+# Define a list of fruits
+fruits = ["apple", "banana", "orange", "kiwi", "grape"]
+
+# Using a for loop to iterate over the fruits list
+print("Using a for loop:")
+for fruit in fruits:
+    print(fruit)
+
+# Using a while loop to iterate over the fruits list
+print("\nUsing a while loop:")
+index = 0
+while index < len(fruits):
+    print(fruits[index])
+    index += 1
