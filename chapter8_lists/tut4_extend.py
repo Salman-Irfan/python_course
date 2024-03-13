a2a25e495759c986f8cf26ea8ae93689a19f15a1
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-13 12:38:42 +0500

    list extend method for concatenation

---
+++ chapter8_lists/tut4_extend.py
@@ -0,0 +1,14 @@
+# Define two lists
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+# Concatenate the lists using the + operator
+concatenated_list = list1 + list2
+
+print(concatenated_list)
+
+
+# Extend list1 with the elements of list2 using the extend() method
+list1.extend(list2)
+
+print(list1)
