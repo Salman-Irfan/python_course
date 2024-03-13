0debe376f75063048f9856bb07a0039b825bf4a7
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-13 12:18:02 +0500

    lists

---
+++ chapter8_lists/tut1_lists.py
@@ -0,0 +1,20 @@
+# numbers
+numbers = [1, 2, 3]
+print(numbers)
+
+# words
+words = ["abc", "def", "ghi"]
+print(words)
+
+# mix / all data types
+mix_data_structures = [
+    1,
+    2.5,
+    "hello",
+    True,
+    [4, 5, 6],
+    {"name": "John", "age": 30},
+    (7, 8, 9),
+    None,
+]
+print(mix_data_structures)
