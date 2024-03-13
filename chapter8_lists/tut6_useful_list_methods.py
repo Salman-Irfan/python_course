f8e42824605131f045e63003c6bcec43842a8643
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-13 15:59:38 +0500

    some useful list methods

---
+++ chapter8_lists/tut6_useful_list_methods.py
@@ -0,0 +1,14 @@
+# Initial list of fruits
+fruits = ["apple", "banana", "orange", "grape", "apple"]
+numbers = [4, 6, 1, 7, 2, 9, 4, 7]
+# count
+print(fruits.count("apple"))  # 2
+
+# sort in alphabetical order
+fruits.sort()
+print(fruits)
+
+# sorted numbers
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+
