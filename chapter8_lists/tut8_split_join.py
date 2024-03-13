dad55e329778836572e453852b7233b15755d53b
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-13 16:31:54 +0500

    split and join list method

---
+++ chapter8_lists/tut8_split_join.py
@@ -0,0 +1,10 @@
+# split
+# convert string into list
+user_info = "salman 25"
+user_info_split = user_info.split()
+print(user_info_split)
+
+# join
+# convert list into string
+user_info_join = ",".join(user_info_split)
+print(user_info_join)
