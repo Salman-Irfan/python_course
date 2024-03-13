a9d9230a5b653619727e24b1ea4a06fa44531f1a
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-13 12:34:15 +0500

    list insert method

---
+++ chapter8_lists/tut3_insert.py
@@ -0,0 +1,9 @@
+# Initial list of fruits
+fruits = ["apple", "banana", "orange", "grape"]
+
+# Inserting a new fruit at index 2
+new_fruit = "kiwi"
+insert_index = 2
+fruits.insert(insert_index, new_fruit)
+
+print(fruits)
