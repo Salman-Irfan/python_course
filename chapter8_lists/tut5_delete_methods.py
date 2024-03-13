e1f456f6db70aeafa8382278a2eaf570efdf9355
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-13 12:48:44 +0500

    pop and remove ist methods

---
+++ chapter8_lists/tut5_delete_methods.py
@@ -0,0 +1,15 @@
+# mix / all data types
+mix_data_structures = [
+    1,
+    2.5,
+    "hello",
+    True,
+    [4, 5, 6],
+    {"name": "John", "age": 30},
+    (7, 8, 9),
+    None,
+]
+mix_data_structures.pop() # remove from last
+mix_data_structures.pop(1) # remove from index
+mix_data_structures.remove("hello") # remove by value
+print(mix_data_structures)
