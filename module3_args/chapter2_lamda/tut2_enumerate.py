f05efb30e2ec3566c6c2d8c3de98fb6cd7f76701
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-15 15:36:53 +0500

    lamda, enumerate, map

---
+++ module3_args/chapter2_lamda/tut2_enumerate.py
@@ -0,0 +1,5 @@
+fruits = ["apple", "banana", "orange", "kiwi"]
+
+# Using enumerate to print fruits along with their index
+for index, fruit in enumerate(fruits):
+    print(f"Index {index}: {fruit}")
---
+++ module3_args/chapter2_lamda/tut3_map.py
@@ -0,0 +1,3 @@
+nums = [1, 2, 3, 4, 5]
+result = list(map(lambda num: num, nums))
+print(result)
