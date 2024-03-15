8cff20903c75de2a2ec955329be8a63bafa51944
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-15 11:43:54 +0500

    list comprehension examples

--- module2_python_short_syntaxes/tut1_list_comprehension.py
+++ module2_python_short_syntaxes/tut1_list_comprehension.py
@@ -1,2 +1,11 @@
+# simple
+simple = [i for i in range(1,11)]
+print (simple)
+
+# square
 square = [i**2 for i in range(1,11)]
 print (square)
+
+# negative list
+negatve = [-i for i in range(1,11)]
+print (negatve)
