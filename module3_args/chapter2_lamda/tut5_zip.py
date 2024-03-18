8d65125b7eba985ef85b03dce8077e78be9f1f60
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-18 14:31:23 +0500

    lists from zip, fix

--- module3_args/chapter2_lamda/tut5_zip.py
+++ module3_args/chapter2_lamda/tut5_zip.py
@@ -5,9 +5,9 @@ print(list(zip(user_ids, user_names)))
 print(dict(zip(user_ids, user_names)))
 
 
-list = [(1, 2), (3, 4), (5, 6), (7, 8)]
+list_tuple = [(1, 2), (3, 4), (5, 6), (7, 8)]
 
-l1, l2 = list(zip(*list))
+l1, l2 = list(zip(*list_tuple))
 
 print(l1)
 print(l2)
