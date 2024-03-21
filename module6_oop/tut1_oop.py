892aa5de7e3931855acd174c8ab15b3ee1acf1a1
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-21 18:09:35 +0500

    laptop class and instances

--- module6_oop/tut1_oop.py
+++ module6_oop/tut1_oop.py
@@ -1,5 +1,6 @@
 class Person:
     def __init__(self, first_name, last_name, age):
+        print (f"init method or constructor method called")
         # instance variables
         self.first_name = first_name
         self.last_name = last_name
@@ -9,3 +10,27 @@ person1 = Person('salman', 'irfan', 25)
 
 print (person1)
 print (person1.first_name)
+
+# laptop class
+class Laptop:
+    def __init__(self, brand_name, model_name, price):
+        # instance variables
+        self.brand_name = brand_name
+        self.model_name = model_name
+        self.price = price
+
+# laptop instances
+# Create two instances of the Laptop class
+laptop1 = Laptop("HP", "Pavilion", 1200)
+laptop2 = Laptop("Dell", "Inspiron", 1500)
+
+# Accessing attributes of the laptop instances
+print("Laptop 1:")
+print("Brand Name:", laptop1.brand_name)
+print("Model Name:", laptop1.model_name)
+print("Price:", laptop1.price)
+
+print("\nLaptop 2:")
+print("Brand Name:", laptop2.brand_name)
+print("Model Name:", laptop2.model_name)
+print("Price:", laptop2.price)
