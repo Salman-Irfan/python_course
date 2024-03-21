a1fe270ce9e4606a97a673d02a3a50bc2f661990
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-21 17:40:38 +0500

    time comparison between list and generator comprehension && git push

---
+++ module5_generators/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+ared_greater_than_5_list:
+#     print(squared_num)
--- module5_generators/tut2_list_vs_generators.py
+++ module5_generators/tut2_list_vs_generators.py
@@ -6,11 +6,22 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # List comprehension to generate squared values of numbers greater than 5
 start_time = time.time()
-squared_greater_than_5_list = [num**2 for num in range(1,1000000000) if num > 5]
+squared_greater_than_5_list = [num**2 for num in range(1,10000) if num > 5]
 end_time = time.time()
 
 # Printing the squared values
-for squared_num in squared_greater_than_5_list:
-    print(squared_num)
+# for squared_num in squared_greater_than_5_list:
+#     print(squared_num)
+
+print("Time taken with list comprehension:", end_time - start_time)
+
+# generator comprehension to generate squared values of numbers greater than 5
+start_time = time.time()
+squared_greater_than_5_list = (num**2 for num in range(1,10000) if num > 5)
+end_time = time.time()
+
+# Printing the squared values
+# for squared_num in squared_greater_than_5_list:
+#     print(squared_num)
 
 print("Time taken with list comprehension:", end_time - start_time)
