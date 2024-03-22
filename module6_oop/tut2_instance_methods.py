ae6c012f4b03d655fe23794148c98ee901ddc1b9
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-22 11:39:06 +0500

    instance methods

---
+++ module6_oop/tut2_instance_methods.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    # method to get full name
+    def get_full_name(self):
+        print(f"{self.first_name} {self.last_name}")
+
+salman = Person('salman', 'irfan', 25)
+salman.get_full_name()
